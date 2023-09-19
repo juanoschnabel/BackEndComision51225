75c6044109bdcff33850e1e9cc6c03bdc3b8cbae
juanoschnabel <juanmschnabel@gmail.com>
2023-09-19 14:20:11 -0300

    eliminacion de usuarios vencidos

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,11 +1,12 @@
 import { userModel } from "../models/Users.js";
+import { DateTime } from "luxon";
 
 class UserService {
   async getUsers(req, res) {
     try {
       const getUsers = await userModel.find();
       const users = getUsers.map(
-        ({ first_name, last_name, email, age, role, cart, _id }) => ({
+        ({
           first_name,
           last_name,
           email,
@@ -13,6 +14,18 @@ class UserService {
           role,
           cart,
           _id,
+          user_creation_date,
+          last_login,
+        }) => ({
+          first_name,
+          last_name,
+          email,
+          age,
+          role,
+          cart,
+          _id,
+          user_creation_date,
+          last_login,
         })
       );
       res.render("sessions/users", {
@@ -35,6 +48,40 @@ class UserService {
         role,
         cart,
         _id,
+      })
+    );
+    res.render("sessions/users", {
+      users: users,
+    });
+  }
+  async deleteOldUsers(req, res) {
+    const twoHoursAgo = new Date();
+    twoHoursAgo.setHours(twoHoursAgo.getMinutes() - 2);
+    await userModel.deleteMany({
+      last_login: { $lt: twoHoursAgo },
+    });
+    const getUsers = await userModel.find();
+    const users = getUsers.map(
+      ({
+        first_name,
+        last_name,
+        email,
+        age,
+        role,
+        cart,
+        _id,
+        user_creation_date,
+        last_login,
+      }) => ({
+        first_name,
+        last_name,
+        email,
+        age,
+        role,
+        cart,
+        _id,
+        user_creation_date,
+        last_login,
       })
     );
     res.render("sessions/users", {
