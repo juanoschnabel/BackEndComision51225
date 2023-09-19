aa9990be550f8e49afcf4f3fd8855621316a4667
juanoschnabel <juanmschnabel@gmail.com>
2023-09-19 15:06:32 -0300

    correccion

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { userService } from "../services/user.services.js";
 const usersRouter = Router();
-sessionRouter.get("/", (req, res) => {
+usersRouter.get("/", (req, res) => {
   userService.traerUsuariosSinBorrar(req, res);
 });
 // usersRouter.delete("/", (req, res) => {
